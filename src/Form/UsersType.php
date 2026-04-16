37aec739bd360367cd01bc00571a499abd3c9dcf
Neo Mudau <Neo.mudau@mfundopedia.co.za>
2026-04-16 15:20:12 +0200

    fixed a bug on userType form

--- src/Form/UsersType.php
+++ src/Form/UsersType.php
@@ -2,7 +2,7 @@
 
 namespace App\Form;
 
-use App\Entity\Users;
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -21,7 +21,7 @@ class UsersType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Users::class,
+            'data_class' => User::class,
         ]);
     }
 }
